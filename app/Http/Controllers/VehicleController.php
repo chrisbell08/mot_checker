5da4df34e94f2d9d7753cb67e12b16088a422f9c
Chris Bell <chris@mangohill.net>
2015-10-28 11:22:32 +0000

    Fixed PSR-2 errors in Controllers

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -1,16 +1,16 @@
-<?php namespace App\Http\Controllers;
+<?php
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Session, Redirect;
 
-class VehicleController extends Controller {
-
-	/**
-	 * [__construct description]
-	 * @param [type] $repository [description]
-	 */
-	public function __construct(  ) {
-		$this->repository = app('App\Http\Interfaces\VehicleInterface');
-	}
-
+class VehicleController extends Controller
+{
+    /**
+     * [__construct description]
+     * @param [type] $repository [description]
+     */
+    public function __construct()
+    {
+        $this->repository = app('App\Http\Interfaces\VehicleInterface');
+    }
 }
--- app/Http/Controllers/VehicleLookupController.php
+++ app/Http/Controllers/VehicleLookupController.php
@@ -1,4 +1,5 @@
-<?php namespace App\Http\Controllers;
+<?php
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Models\Vehicle;
@@ -6,102 +7,101 @@ use App\Http\Models\VehicleLookup;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\View;
-use Session, Redirect;
 
-class VehicleLookupController extends Controller {
+class VehicleLookupController extends Controller
+{
+    /**
+     * [$viewPath description]
+     * @var string
+     */
+    protected $viewPath = 'VehicleLookup.';
 
-	/**
-	 * [$viewPath description]
-	 * @var string
-	 */
-	protected $viewPath = 'VehicleLookup.';
+    /**
+     * [__construct description]
+     * @param [type] $repository [description]
+     */
+    public function __construct(VehicleLookup $model)
+    {
+        $this->repository = app('App\Http\Interfaces\VehicleLookupInterface');
+        $this->model = $model;
+        $this->vehicleRepository = app('App\Http\Interfaces\VehicleInterface');
+    }
 
-	/**
-	 * [__construct description]
-	 * @param [type] $repository [description]
-	 */
-	public function __construct(VehicleLookup $model ) {
-		$this->repository = app('App\Http\Interfaces\VehicleLookupInterface');
-		$this->model = $model;
-		$this->vehicleRepository = app('App\Http\Interfaces\VehicleInterface');
-	}
+    /*
+     * New lookup Form
+     *
+     * @param
+     *
+     * @return view
+     */
+    public function newLookupForm()
+    {
+        return view::make($this->viewPath . 'new');
+    }
 
-	/*
-	 * New lookup Form
-	 *
-	 * @param
-	 *
-	 * @return view
-	 */
-	public function newLookupForm()
-	{
-		return view::make($this->viewPath . 'new');
-	}
+    /*
+     * Receive post data for new lookup and return results
+     *
+     * @param $_POST['reg']:string
+     * @param $_POST['make']:string
+     *
+     * @return JSON
+     */
+    public function postNewLookup()
+    {
+        // Set the user to 0 if not logged in (for api)
+        (Auth::user())? $userId = Auth::user()->id : $userId = 0;
 
-	/*
-	 * Receive post data for new lookup and return results
-	 *
-	 * @param $_POST['reg']:string
-	 * @param $_POST['make']:string
-	 *
-	 * @return JSON
-	 */
-	public function postNewLookup()
-	{
-		// Set the user to 0 if not logged in (for api)
-		(Auth::user())? $userId = Auth::user()->id : $userId = 0;
+        $reg = str_replace(' ', '', Input::get('reg'));
 
-		$reg = str_replace(' ', '', Input::get('reg'));
+        // Lookup details
+        $lookup = $this->repository->newLookup($userId, $reg, Input::get('make'));
 
-		// Lookup details
-		$lookup = $this->repository->newLookup($userId, $reg, Input::get('make'));
+        if ($lookup) {
+            return view::make($this->viewPath . 'partials.newLookup')->with('lookup', $lookup);
+        } else {
+            return view::make($this->viewPath . 'partials.newLookup');
+        }
+    }
 
-		if($lookup) {
-			return view::make($this->viewPath . 'partials.newLookup')->with('lookup', $lookup);
-		} else {
-			return view::make($this->viewPath . 'partials.newLookup');
-		}
+    /*
+     * Get the vehicle details from the id
+     *
+     * @param $id:int
+     * @return view
+     */
+    public function getVehicleDetails($id)
+    {
+        $lookup = $this->model->findorFail($id);
 
-	}
+        if ($lookup) {
+            $lookup = $this->repository->checkDates($lookup);
+        }
 
-	/*
-	 * Get the vehicle details from the id
-	 *
-	 * @param $id:int
-	 * @return view
-	 */
-	public function getVehicleDetails($id)
-	{
-		$lookup = $this->model->findorFail($id);
+        return view::make($this->viewPath . 'partials.newLookup')->with('lookup', $lookup);
+    }
 
-		if($lookup) {
-			$lookup = $this->repository->checkDates($lookup);
-		}
+    /*
+     * Refresh a lookup by the ID
+     *
+     * @param $id:int
+     * @return view
+     */
+    public function refreshLookup($id)
+    {
+        // Get the lookup collection
+        $lookup = $this->model->findOrFail($id);
 
-		return view::make($this->viewPath . 'partials.newLookup')->with('lookup', $lookup);
-	}
+        // Get the vehcile to pass to casperjs
+        $vehicle = Vehicle::findOrFail($lookup->vehicle_id);
 
-	/*
-	 * Refresh a lookup by the ID
-	 *
-	 * @param $id:int
-	 * @return view
-	 */
-	public function refreshLookup($id)
-	{
-		// Get the lookup collection
-		$lookup = $this->model->findOrFail($id);
+        // perform lookup
+        $this->repository->newLookup(Auth::user()->id, $vehicle->reg, $vehicle->make);
 
-		// Get the vehcile to pass to casperjs
-		$vehicle = Vehicle::findOrFail($lookup->vehicle_id);
+        $vehicle = $this->vehicleRepository->getVehicleById($vehicle->id);
 
-		// perform lookup
-		$this->repository->newLookup(Auth::user()->id, $vehicle->reg, $vehicle->make);
+        $vehicle->icon = $this->vehicleRepository->getIcon($vehicle);
 
-		$vehicle = $this->vehicleRepository->getVehicleById($vehicle->id);
-
-		$vehicle->icon = $this->vehicleRepository->getIcon($vehicle);
-
-		return view::make('partials.vehicleTable')->with('vehicle', $vehicle);
-	}
+        return view::make('partials.vehicleTable')->with('vehicle', $vehicle);
+    }
 }
